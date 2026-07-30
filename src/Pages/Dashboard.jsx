6dc96810b6d1eace1a82d78be1b7e7bc23fc1b6b
ezekiel <favourtobiloba200@gmail.com>
2026-07-30 10:03:09 +0100

    useeffect

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,10 +1,49 @@
+import { useEffect, useState } from "react"
 
 
 export const Dashboard = () => {
+    const [timer, setTimer] = useState(0)
+    const [product, setProduct] = useState(null)
+
+
+    const handleProduct = async () => {
+        try {
+            const response = await fetch("https://fakestoreapi.com/products")
+            const data = await response.json()
+            console.log("data", data);
+
+            if (data) {
+                setProduct(data)
+            }
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+
+
+    useEffect(() => {
+        handleProduct()
+    }, [timer])
+
     return (
         <div>
             <h1>Dashboard</h1>
-            <p>  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum pariatur molestiae neque distinctio nam assumenda impedit officia facilis voluptates quam doloribus iusto consectetur vero sunt, id veniam a minima? Veniam.</p>
+            <button onClick={() => setTimer(timer + 1)}>Refetch</button>
+            <p>{timer}</p>
+
+            <div>
+                {product && <div>
+                    {product.map((pro) => (
+                        <ul style={{ border: "thin solid black", margin: "10px", padding: "10px", listStyleType: "none", width: "300px", display: "inline-block" }} key={pro.id}>
+                            <li style={{ fontSize: "25px", fontWeight: "bolder" }}>{pro.title}</li>
+                            <li><img style={{ width: "40%" }} src={pro.image} alt={pro.title.slice(10)} /></li>
+                            <li>{pro.description}</li>
+                            <li>{pro.price}</li>
+                        </ul>
+                    ))}
+                </div>}
+            </div>
         </div>
     )
 }
